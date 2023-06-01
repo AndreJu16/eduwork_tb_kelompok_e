eef0886acfbe6ebdc6e884ba3744830232edcd65
Evi Dianasari <evidnsr@gmail.com>
2023-06-01 21:32:24 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -53,9 +53,9 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="about.php">About</a></li>
           
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto " href="portfolio.php">Portfolio</a></li>
           
           <li class="dropdown"><a href="#"><span>Kategori</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
