028533abae8f1bade0aef948a30fee9cb87154b3
Evi Dianasari <evidnsr@gmail.com>
2023-06-04 23:44:43 +0800

    change tab title in head

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>eTravel Bootstrap Template - Index</title>
+  <title>eTravel - Home</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
